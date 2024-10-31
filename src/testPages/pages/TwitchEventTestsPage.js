be378124e9c61e9c4eae3881b4f05a3df182c7e0
Tom Norman <tom88norman@hotmail.co.uk>
2024-10-31 21:49:13 +0000

    put auth block on /test

--- src/testPages/pages/TwitchEventTestsPage.js
+++ src/testPages/pages/TwitchEventTestsPage.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NewSubscriberPayloadTester from '../components/NewSubscriberPayloadTester'
 import ResubPayloadTester from '../components/ResubPayloadTester'
 import GiftSubPayloadTester from '../components/GiftSubPayloadTester'
+import { checkAuth } from '../../httpRequests/checkAuth'
 
 const TwitchEventTestsPage = () => {
   const [selectedEvent, setSelectedEvent] = useState('new_subscriber')
+
+  useEffect(() => {
+    const checkSession = async () => {
+        try {
+            const authenticated = await checkAuth();
+            if (!authenticated){
+              alert("not authorized")
+              window.location.href = '/';
+            } 
+        } catch(error) {
+            console.error("Error checking session: ", error)
+        }              
+    }
+    checkSession();
+}, []);
 
   const renderTesterComponent = () => {
     switch (selectedEvent) {
